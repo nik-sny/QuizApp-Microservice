02aae06c6c44a2e40cd35f6a1b3e074f2d292a4c
nkl-sny <nikhilsanjay43@gmail.com>
2024-09-18 09:19:43 +0530

    microservice updated

---
+++ apigateway/src/test/java/com/nikhil/apigateway/ApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhil.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ question-service/src/test/java/com/nikhil/question_service/QuestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhil.question_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
